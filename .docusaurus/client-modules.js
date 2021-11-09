6b0d5a5cbf317a5701385fe3fb360c75a1902a5d
LauraVav <91527376+LauraVav@users.noreply.github.com>
2021-11-09 16:06:39 +0100

    removed additional files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/lauravavasseur/Desktop/work/code/git/documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/lauravavasseur/Desktop/work/code/git/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/lauravavasseur/Desktop/work/code/git/documentation/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/lauravavasseur/Desktop/work/code/git/documentation/src/css/custom.css"),
-];
