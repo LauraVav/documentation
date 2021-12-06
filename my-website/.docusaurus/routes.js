19cf5ebb7b5131ff9119f441a1a0e648ecd778ef
LauraVav <91527376+LauraVav@users.noreply.github.com>
2021-12-06 11:07:50 +0100

    Added 3 SDK tutorials

--- my-website/.docusaurus/routes.js
+++ my-website/.docusaurus/routes.js
@@ -4,41 +4,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug','3d6'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config','914'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content','c28'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData','3cf'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata','31b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry','0da'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes','244'),
-    exact: true
-  },
-  {
     path: '/blog/archive',
     component: ComponentCreator('/blog/archive','f4c'),
     exact: true
@@ -50,7 +15,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs','a80'),
+    component: ComponentCreator('/docs','57a'),
     routes: [
       {
         path: '/docs/API/coreference',
@@ -379,6 +344,24 @@ export default [
       {
         path: '/docs/Tutorials/API/tokenizer',
         component: ComponentCreator('/docs/Tutorials/API/tokenizer','a07'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/Tutorials/Python SDK/get-emotion',
+        component: ComponentCreator('/docs/Tutorials/Python SDK/get-emotion','210'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/Tutorials/Python SDK/get-sentiment-CS',
+        component: ComponentCreator('/docs/Tutorials/Python SDK/get-sentiment-CS','76f'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/Tutorials/Python SDK/list-entities',
+        component: ComponentCreator('/docs/Tutorials/Python SDK/list-entities','515'),
         exact: true,
         'sidebar': "tutorialSidebar"
       },
